cb4b60deac2cd8d886f3cfc9ce749abe6c1f30d2
shantanu <shantanuhon99@gmail.com>
2024-11-30 14:24:27 +0530

    .

--- resources/views/testimonial.blade.php
+++ resources/views/testimonial.blade.php
@@ -6,8 +6,8 @@
 <figure class="snip1390">
     <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS2TgOv9CMmsUzYKCcLGWPvqcpUk6HXp2mnww&s" alt="profile-sample3" class="profile" />
     <figcaption>
-        <h3 style="color: white;">Ratan Petkar</h3>
-        <blockquote>“Using this service has been an absolute game-changer! The attention to detail and professionalism were top-notch. Highly recommended for anyone looking for quality and reliability!”</blockquote>
+        <h3 style="color: white;">Rutuja Shelke</h3>
+        <blockquote>“Data Doctor made recovering my lost files easy and quick. I managed to get back all my important documents without any issues. Highly recommend it!”</blockquote>
     </figcaption>
     <div class="star-rating">
         <span class="star filled">★</span>
@@ -22,8 +22,8 @@
 <figure class="snip1390 hover">
     <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS2TgOv9CMmsUzYKCcLGWPvqcpUk6HXp2mnww&s" alt="profile-sample5" class="profile" />
     <figcaption>
-        <h3 style="color: white;">Sahil J. Nimse</h3>
-        <blockquote>“Absolutely outstanding! The service exceeded all my expectations, and I couldn't be happier with the results. Flawless experience from start to finish.”</blockquote>
+        <h3 style="color: white;">Kaushal Jare</h3>
+        <blockquote>“I deleted important files by mistake, but Data Doctor restored everything quickly. It’s a total lifesaver!”</blockquote>
     </figcaption>
     <div class="star-rating">
         <span class="star filled">★</span>
@@ -38,8 +38,8 @@
 <figure class="snip1390">
     <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS2TgOv9CMmsUzYKCcLGWPvqcpUk6HXp2mnww&s" alt="profile-sample6" class="profile" />
     <figcaption>
-        <h3 style="color: white;">Rutuja Shelke</h3>
-        <blockquote>“A solid experience overall. Great support and a user-friendly process. There's a bit of room for improvement, but I'm definitely satisfied with the outcome.”</blockquote>
+        <h3 style="color: white;">Ratan Petkar</h3>
+        <blockquote>“Data Doctor recovered my lost photos and videos easily. The process was simple and effective.”</blockquote>
     </figcaption>
     <div class="star-rating">
         <span class="star filled">★</span>
@@ -50,5 +50,6 @@
     </div>
     <p style="color:white;">4 Star Rating</p>
 </figure>
+
 
 </div>
